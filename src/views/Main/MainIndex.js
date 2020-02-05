131ac88ebc6dcce2f08ee42d03111f8f170b7d75
Mark Abrogar <greem.reaper008@gmail.com>
2020-02-05 17:17:30 +0800

    Dashboard2

--- src/views/Main/MainIndex.js
+++ src/views/Main/MainIndex.js
@@ -7,7 +7,6 @@ import {
 	Row,
 	Card,
 	CardBody,
-	CardHeader,
 	Table
 } from 'reactstrap';
 import StyledCardHeader from '../../components/StyledCardHeader';
@@ -17,41 +16,41 @@ class MainIndex extends Component {
 	render() {
 		
 		return (
-			<Container fluid>
+			<Container fluid style={{paddingLeft:'7px',paddingRight:'7px'}}>
 				<Row>
 					<Col className="sm-6 lg-3">
-						<Card>
+						<Card style={{  padding: 10 }}>
 							<CardBody className="clearfix p-0">
 								<a className="bg-blue p-4 mr-3 float-left">
 									<i className="fa fa-money fa-3x"></i>
 								</a>
-								<h5 className="mb-0 text-primary pt-3"> $1,995.5</h5>
-								<h5 className="text-muted text-uppercase font-weight-bold font-xs"> Total Income </h5>
-								<h6 className="text-muted font-weight-bold font-xs">Recievables -> $0.00/$0.00</h6>
+								<h5 className="mb-0 text-right text-primary pt-3"> $1,995.5</h5>
+								<h5 className="text-muted text-right text-uppercase font-weight-bold font-xs"> Total Income </h5>
+								<h6 className="text-muted text-right font-weight-bold font-xs">Recievables -> $0.00/$0.00</h6>
 							</CardBody>
 						</Card>
 					</Col>
 					<Col className="sm-6 lg-3">
-						<Card>
+						<Card style={{  padding: 10 }}>
 							<CardBody className="clearfix p-0">
 								<a className="bg-red p-4 mr-3 float-left">
 									<i className="fa fa-shopping-cart fa-3x"></i>
 								</a>
-								<h5 className="mb-0 text-primary pt-3"> $1,995.5</h5>
-								<h5 className="text-muted text-uppercase font-weight-bold font-xs"> Total Expenses </h5>
-								<h6 className="text-muted font-weight-bold font-xs">Payables -> $0.00/$0.00</h6>
+								<h5 className="mb-0 text-right text-primary pt-3"> $1,995.5</h5>
+								<h5 className="text-muted text-right text-uppercase font-weight-bold font-xs"> Total Expenses </h5>
+								<h6 className="text-muted text-right font-weight-bold font-xs">Payables -> $0.00/$0.00</h6>
 							</CardBody>
 						</Card>
 					</Col>
 					<Col className="sm-6 lg-3">
-						<Card>
+						<Card style={{  padding: 10 }}>
 							<CardBody className="clearfix p-0">
 								<a className="bg-green p-4 mr-3 float-left">
 									<i className="fa fa-heart fa-3x"></i>
 								</a>
-								<h5 className="mb-0 text-primary pt-3"> $1,995.5</h5>
-								<h5 className="text-muted text-uppercase font-weight-bold font-xs"> Total Profit </h5>
-								<h6 className="text-muted font-weight-bold font-xs">Upcoming -> $0.00/$0.00</h6>
+								<h5 className="mb-0 text-right text-primary pt-3"> $1,995.5</h5>
+								<h5 className="text-muted text-right text-uppercase font-weight-bold font-xs"> Total Profit </h5>
+								<h6 className="text-muted text-right font-weight-bold font-xs">Upcoming -> $0.00/$0.00</h6>
 							</CardBody>
 						</Card>
 					</Col>
